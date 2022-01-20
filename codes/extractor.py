ffd5f1e667b0e89cd7f76ae7a7097ab8c2699b24
junjun1023 <jjjunpersonal@gmail.com>
2022-01-20 01:22:59 +0000

    Fix: zero loss of two inputs

--- codes/extractor.py
+++ codes/extractor.py
@@ -5,6 +5,12 @@ from typing import Dict, Iterable, Callable
 
 class Extractor(nn.Module):
     
+    """
+    usage:
+    feature_list = ['features_13','features_23','features_33','features_43']
+    extract_model = Extractor(model=pretrained_model,layers=feature_list).cuda()
+    """
+    
     def __init__(self, model: nn.Module=None, layers: Iterable[str]=None):
         super().__init__()
         self.model = model
@@ -12,14 +18,16 @@ class Extractor(nn.Module):
         if self.model == None :
             self.model =  models.vgg16(pretrained=True)
         self.model = self.model.eval()
-        
+  
         for param in self.model.parameters():
             param.requires_grad = False
-        
+
         for name, layer in self.model.named_children():
+            
             if isinstance(layer, nn.Sequential):
                 for children_name, children_layer in layer.named_children():
                     children_layer.__name__ = name + '_' + children_name
+
                     if children_layer.__name__ in layers:
                         children_layer.register_forward_hook(
                             self.save_output()
@@ -37,6 +45,7 @@ class Extractor(nn.Module):
             self._features[layer.__name__] = output
         return fn
     
-    def forward(self,x):
+    def forward(self, x):
+        self.model = self.model.to(x.device)
         _ = self.model(x)
         return self._features
