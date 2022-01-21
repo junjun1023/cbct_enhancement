a13fb4c7dff18f417d5949e32ab3c8eafd4b580f
junjun1023 <jjjunpersonal@gmail.com>
2022-01-21 22:51:09 +0000

    fix: module not found error and scale range of processed bone to [0, 1]

---
+++ codes/generator.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+import segmentation_models_pytorch as smp
+from typing import Dict, Iterable, Callable
+
+class Generator():
+    
+    """
+    usage:
+    feature_list = ['features_13','features_23','features_33','features_43']
+    extract_model = Extractor(model=pretrained_model,layers=feature_list).cuda()
+    """
+    
+    def __init__(self, framework="unet", ):
+        super().__init__()
+        self.model = model
+        self._features = {}
+        if self.model == None :
+            self.model =  models.vgg16(pretrained=True)
+        self.model = self.model.eval()
+        
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        for name, layer in self.model.named_children():
+            
+            if isinstance(layer, nn.Sequential):
+                for children_name, children_layer in layer.named_children():
+                    children_layer.__name__ = name + '_' + children_name
+
+                    if children_layer.__name__ in layers:
+                        children_layer.register_forward_hook(
+                            self.save_output()
+                        )
+                        
+            else:
+                layer.__name__ = name
+                if layer.__name__ in layers: 
+                    layer.register_forward_hook(
+                        self.save_output()
+                    )
+                    
+    def save_output(self):
+        def fn(layer, _, output):
+            self._features[layer.__name__] = output
+        return fn
+    
+    def forward(self, x):
+        _ = self.model(x)
+        return self._features
