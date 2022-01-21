42d2366ef94f7cf7a3eaf06d5d25bba3a5a8c0a2
junjun1023 <jjjunpersonal@gmail.com>
2022-01-21 07:02:15 +0000

    fix preprocessing

--- codes/dataset.py
+++ codes/dataset.py
@@ -7,70 +7,75 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset as BaseDataset
 
-from .utils import read_npy, valid_slices, hu_clip, get_mask, bounded, min_max_normalize
+from .utils import hu_clip, read_dicom, valid_slices
+from .augmentations.mask import get_mask
+from .augmentations.air_bone_mask import get_air_bone_mask
 
 
 class Dataset(BaseDataset):
     """
     Args:
         path (str): path to dataset
-        x_channel (int): number of channels for X(images)
-        y_channel (int): number of channels for y(masks)
-        do_resize (tuple of ints): resize the image and mask, if None, DO NOT do resize
         augmentation (albumentations.Compose): data transfromation pipeline 
             (e.g. flip, scale, etc.)
         preprocessing (albumentations.Compose): data preprocessing 
             (e.g. noralization, shape manipulation, etc.)
     """
-    def __init__(self, path, intensity_aug=None, geometry_aug=None, preprocessing=None, tolerance=20):
+    def __init__(self, path, geometry_aug=None):
         paths = sorted(glob.glob(path))
         self.xs = []
         self.ys = []
-        self.air = []
-        self.bone = []
         
         # read cbct and ct
-        for i in range(0, len(paths), 8):
-            # air, bone, img, mask
-            air = read_npy(paths[i])
-            bone = read_npy(paths[i+1])
-            cbct_slices =  read_npy(paths[i+2])
-            ct_slices = read_npy(paths[i]+6)
+        for i in range(0, len(paths), 2):
+            cbct_slices = read_dicom(paths[i])
+            ct_slices = read_dicom(paths[i+1])
 
-            region = valid_slices(cbct, ct)
+            region = valid_slices(cbct_slices, ct_slices)
             # ditch first and last 3
             self.xs = self.xs + cbct_slices[region[0] + 3: region[1] - 3]
             self.ys = self.ys + ct_slices[region[0] + 3: region[1] - 3]
-            self.air = self.air + air[region[0] + 3: region[1] - 3]
-            self.bone = self.bone + bone[region[0] + 3: region[1] - 3]
             
-        
-        self.intensity_aug = intensity_aug
+
         self.geometry_aug = geometry_aug
-        self.preprocessing = preprocessing
 
         
     
     def __getitem__(self, i):
 
         # read img
-        x = self.xs[i]
-        y = self.ys[i]
-        air = self.air[i]
-        bone = self.bone[i]
+        x = self.xs[i].pixel_array.copy()
+        y = self.ys[i].pixel_array.copy()
 
+        mask_x = get_mask()(image=x)["image"]
+        mask_y = get_mask()(image=y)["image"]
+ 
+        x = hu_clip(x, 500, -500, True)
+        y = hu_clip(y, 500, -500, True)
+
+        x = x * mask_x
+        y = y * mask_y
+
+        sample = get_air_bone_mask()(image=x)["image"]
+        air_x, bone_x = sample[0, :, :], sample[1, :, :]
+        sample = get_air_bone_mask()(image=y)["image"]
+        air_y, bone_y = sample[0, :, :], sample[1, :, :]
+            
+            
         if self.geometry_aug:
-            sample = self.geometry_aug(image=x, image0=y, image1=air, image2=bone)
-            x, y, air, bone = sample["image"], sample["image0"], sample["image1"], sample["image2"]
+            sample = self.geometry_aug(image=x, image0=y, image1=air_x, image2=air_y, image3=bone_x, image4=bone_y)
+            x, y, air_x, air_y, bone_x, bone_y = sample["image"], sample["image0"], sample["image1"], sample["image2"], sample["image3"], sample["image4"]
+            
             
         x = np.expand_dims(x, 0).astype(np.float32)
         y = np.expand_dims(y, 0).astype(np.float32)
-        air = np.expand_dims(air, 0).astype(np.float32)
-        bone = np.expand_dims(bone, 0).astype(np.float32)
+        air_x = np.expand_dims(air_x, 0).astype(np.float32)
+        air_y = np.expand_dims(air_y, 0).astype(np.float32)
+        bone_x = np.expand_dims(bone_x, 0).astype(np.float32)
+        bone_y = np.expand_dims(bone_y, 0).astype(np.float32)      
 
-        return x, y, air, bone
+        return x, y, air_x, air_y, bone_x, bone_y
     
         
     def __len__(self):
         return len(self.xs)
-    
