1e814636577f460daeb621350b5dc285ab8d16ab
junjun1023 <jjjunpersonal@gmail.com>
2022-01-22 07:49:07 +0000

    add: ada belief optimizer

--- codes/optimizer.py
+++ codes/optimizer.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-
+from adabelief_pytorch import AdaBelief
 
 def Optimizer(params, name, **kwargs):
 
@@ -12,6 +12,8 @@ def Optimizer(params, name, **kwargs):
                 return torch.optim.Adam(params, **kwargs)
         elif name == 'sgd':
                 return torch.optim.SGD(params, **kwargs)
+        elif name == "adabelief":
+                return AdaBelief(params, lr=2e-4, eps=1e-12, betas=(0.9,0.999), weight_decouple = True, rectify = False)
         elif callable(name):
                 return name(params, **kwargs)
 
