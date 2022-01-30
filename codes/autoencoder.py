287be30da6001201dd15344015db78543200e88d
junjun1023 <jjjunpersonal@gmail.com>
2022-01-30 18:08:35 +0800

    Fix: syntax error

--- codes/autoencoder.py
+++ codes/autoencoder.py
@@ -4,63 +4,78 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 import segmentation_models_pytorch as smp
-from segmentation_models_pytorch.unet.decoder import DecoderBlock, CenterBlock
 from .activation import Activation
 
 
-class UnetDecoder(nn.Module):
-    def __init__(
-            self,
-            encoder_channels,
-            decoder_channels,
-            n_blocks=5,
-            use_batchnorm=True,
-            attention_type=None,
-            center=False,
-    ):
+
+class DownBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DownBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, padding=1)
+        self.relu  = nn.ReLU()
+        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, padding=1, stride=2)
+    
+    def forward(self, x):
+        return self.conv2(self.relu(self.conv1(x)))
+
+
+class UpBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UpBlock, self).__init__()
+        self.up = nn.Upsample(scale_factor=2, mode="bilinear")
+        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, padding=1)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, padding=1)
+    def forward(self, x):
+        x = self.up(x)
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self, in_channels=1024, out_channels=64):
+        super(Decoder, self).__init__()
+        self.blocks = nn.Sequential(
+            UpBlock(in_channels, 512),
+            UpBlock(512, 256),
+            UpBlock(256, 128),
+            UpBlock(128, out_channels))
+              
+    def forward(self, x):
+        x = self.blocks(x)
+        return x 
+ 
+
+
+class Encoder(nn.Module):
+    def __init__(self, in_channels=1):
+        super(Encoder, self).__init__()
+        self.blocks = nn.Sequential(
+            DownBlock(in_channels, 64),
+            DownBlock(64, 128),
+            DownBlock(128, 256),
+            DownBlock(256, 512),
+            DownBlock(512, 1024))
+
+    def forward(self, x):
+        return self.blocks(x)
+
+
+class AutoEncoder(nn.Module):  # 必要 inherit nn.Module
+    def __init__(self, in_channels=1, head_channels=1):
         super().__init__()
+        
+        self.encoder = Encoder(in_channels)
+        self.decoder = Decoder(1024, 64)
+        self.head = nn.Conv2d(64, head_channels, 3, padding=1)
 
-        if n_blocks != len(decoder_channels):
-            raise ValueError(
-                "Model depth is {}, but you provide `decoder_channels` for {} blocks.".format(
-                    n_blocks, len(decoder_channels)
-                )
-            )
-
-        encoder_channels = encoder_channels[1:]  # remove first skip with same spatial resolution
-        encoder_channels = encoder_channels[::-1]  # reverse channels to start from head of encoder
-
-        # computing blocks input and output channels
-        head_channels = encoder_channels[0]
-        in_channels = [head_channels] + list(decoder_channels[:-1])
-        skip_channels = list(encoder_channels[1:]) + [0]
-        out_channels = decoder_channels
-
-        if center:
-            self.center = CenterBlock(
-                head_channels, head_channels, use_batchnorm=use_batchnorm
-            )
-        else:
-            self.center = nn.Identity()
-
-        # combine decoder keyword arguments
-        kwargs = dict(use_batchnorm=use_batchnorm, attention_type=attention_type)
-        blocks = [
-            DecoderBlock(in_ch, skip_ch, out_ch, **kwargs)
-            for in_ch, skip_ch, out_ch in zip(in_channels, skip_channels, out_channels)
-        ]
-        self.blocks = nn.ModuleList(blocks)
-
-    def forward(self, *features):
-
-        features = features[1:]    # remove first skip with same spatial resolution
-        features = features[::-1]  # reverse channels to start from head of encoder
-
-        head = features[0]
-        skips = features[1:]
-
-        x = self.center(head)
-        for i, decoder_block in enumerate(self.blocks):
-            x = decoder_block(x)
+    def forward(self, x):
+        
+        x = self.encoder(x)
+        x = self.decoder(x)
+        x = self.head(x)
 
         return x
