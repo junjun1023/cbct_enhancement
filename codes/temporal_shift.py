c461e02125ba843737d8fb37e4efea338616d853
junjun1023 <jjjunpersonal@gmail.com>
2022-03-10 08:09:23 +0800

    fix: number of frames not enough for n_segment leads to error

--- codes/temporal_shift.py
+++ codes/temporal_shift.py
@@ -25,6 +25,9 @@ class TemporalShift(nn.Module):
     def shift(x, n_segment, fold_div=3):
         nt, c, h, w = x.size()
         n_batch = nt // n_segment
+        if n_batch == 0:
+            n_batch = 1
+            n_segment = 1
         x = x.view(n_batch, n_segment, c, h, w)
 
         fold = c // fold_div
